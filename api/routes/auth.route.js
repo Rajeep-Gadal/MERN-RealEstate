dab5adef9ae19817e6b7306963ec0dd6550a9cb3
Rajeep-Gadal <rajeepgadal@gmail.com>
2024-05-08 07:43:15 +0545

    created an sigin UI and put some functionality and logged in by the exiting user

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { signup } from "../controllers/auth.controller.js";
+import { signin, signup } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 // now we tryna make a account so we use post method
 router.post("/signup", signup);
+router.post("/signin", signin);
 
 export default router;
