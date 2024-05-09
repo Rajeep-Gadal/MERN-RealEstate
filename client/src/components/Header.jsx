bffde5ceffafe533227bfe2d945d084e748b09e9
Rajeep-Gadal <rajeepgadal@gmail.com>
2024-05-10 05:32:05 +0545

    created an profile pic related to the google account and design the profile image and half make of private profile.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,16 +1,18 @@
 import { FaSearch } from "react-icons/fa";
-import {Link} from "react-router-dom";
-import React from "react";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Header = () => {
+  const { currentUser } = useSelector((state) => state.user);
+
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to="/">
-        <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-          <span className="text-slate-500">Rent</span>
-          <span className="text-slate-700">Ease</span>
-        </h1>
+          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+            <span className="text-slate-500">Rent</span>
+            <span className="text-slate-700">Ease</span>
+          </h1>
         </Link>
         <form className="bg-slate-100 p-3 flex items-center rounded-lg">
           <input
@@ -22,13 +24,26 @@ const Header = () => {
         </form>
         <ul className="flex gap-4">
           <Link to="/">
-          <li className="hidden sm:inline text-slate-700 hover:underline">Home</li>
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              Home
+            </li>
           </Link>
           <Link to="/about">
-          <li className="hidden sm:inline text-slate-700 hover:underline">About</li>
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              About
+            </li>
           </Link>
-          <Link to="/sign-in">
-          <li className="text-slate-700 hover:underline">Sign In</li>
+
+          <Link to="/profile">
+            {currentUser ? (
+              <img
+                src={currentUser.avatar}
+                alt="avatar"
+                className="rounded-full h-8 w-8 object-cover"
+              />
+            ) : (
+              <li className="text-slate-700 hover:underline">Sign In</li>
+            )}
           </Link>
         </ul>
       </div>
