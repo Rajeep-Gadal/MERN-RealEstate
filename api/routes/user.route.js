75d5d5da84004d012b7acabf45a8214727938694
Rajeep-Gadal <rajeepgadal@gmail.com>
2024-06-20 11:15:58 +0545

    Intergrated nodejs listing api from backend to react frontend

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { test, deleteUser, updateUser } from "../controllers/user.controller.js";
+import { test, deleteUser, updateUser, getUserListings } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
@@ -10,5 +10,6 @@ router.post("/update/:id", verifyToken, updateUser);
 // to authorize that only a user that logged in an account can update their account not other users so to do that we use cookies
 router.delete("/delete/:id", verifyToken, deleteUser);
 // this will first verify you that are you a actually user of this account by verifying your token that if matches then redirect to the deleteUser section.
+router.get("/listings/:id", verifyToken, getUserListings)
 
 export default router;
