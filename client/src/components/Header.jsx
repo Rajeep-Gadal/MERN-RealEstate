8e33847cb846b33b4009d881ab374744dea64a87
Rajeep-Gadal <rajeepgadal@gmail.com>
2024-06-23 01:48:41 +0545

    All Changes done

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -32,6 +32,7 @@ const Header = () => {
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to="/">
           <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+            <img src="/brand-logo.png" className="w-[64px]" alt="" />
             <span className="text-slate-500">Rent</span>
             <span className="text-slate-700">Ease</span>
           </h1>
